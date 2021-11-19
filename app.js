ee14032171196daf477c5c488966a437d5c94279
KitchenPizzeria <valomeyrick2012@hotmail.co.uk>
2021-11-19 10:20:21 +0000

    CHanges made

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
+app.get("/", (req, res) => {
   res.send("<h1> JavaScript Gods </h1>");
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
